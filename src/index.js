dd6ad71331c23ce06e872935619a9119d272d45e
Anglesson Araujo <anglesson@outlook.com>
2025-03-23 15:00:23 -0300

    refactor server port configuration to use environment variable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // Create a server with express and listen on port 3000
 import express from 'express';
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
